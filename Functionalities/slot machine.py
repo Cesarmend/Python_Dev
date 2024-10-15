e3f692dcdd9ec14864a42a3120d90a7cb200735b
Cesar <mendezcesarpablo@gmail.com>
2024-10-15 09:47:53 -0500

    new balance-spin

--- Functionalities/slot machine.py
+++ Functionalities/slot machine.py
@@ -13,10 +13,36 @@ def get_payout():
 def main():
     balance = 100
 
-
+    print("**************************")
     print("Welcome to Python Slots")
     print("Symbols: 🍒 🍉 🍋 🔔 ⭐")
+    print("**************************")
 
+    while balance>0 :
+        print(f"Current balance: $ {balance}")
+
+        bet = input("place your bet amount: ")
+
+        if not bet.isdigit():
+            
+            print("Please enter a valid number")
+            print("---------------------------")
+            continue
+
+        bet = int(bet)
+
+        if bet> balance:
+            print("Insuficient funds")
+            print("---------------------------")
+            continue
+
+        if bet <= 0:
+            print("Bet must be greater than 0")
+            continue
+
+        balance -= bet 
+
+        row = spin_row()
 
 if __name__ == '__main__': 
     main()
