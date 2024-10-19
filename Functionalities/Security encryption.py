408e6c7165a640bf9f95b5bd561a2e965ea35a96
Cesar <mendezcesarpablo@gmail.com>
2024-10-19 08:28:43 -0500

    Random shuffle

--- Functionalities/Security encryption.py
+++ Functionalities/Security encryption.py
@@ -1,2 +1,12 @@
 import random
 import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+
+chars = list(chars)
+key= chars.copy()
+
+random.shuffle(key)
+
+print(f"chars: {chars}")
+print(f"key : {key}")
