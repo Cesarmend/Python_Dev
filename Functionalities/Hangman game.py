88b38e87e07f538a2f94f4edbedb1fdee5c66383
Cesar <mendezcesarpablo@gmail.com>
2024-12-19 20:57:34 -0500

    Hangman partA

--- Functionalities/Hangman game.py
+++ Functionalities/Hangman game.py
@@ -38,15 +38,15 @@ def display_man(wrong_guesses):
     print("************")
 
 def display_hint(hint):
-    pass
+    print(" ".join(hint))
 
 def display_answer(answer):
-    pass
+    print(" ".join(answer))
 
 def main():
     answer = random.choice(words)
     hint = ["_"] * len(answer)
-    wrong_guesses = 5
+    wrong_guesses = 0
     guessed_letters = set()
     is_running = True
 
@@ -54,6 +54,37 @@ def main():
         display_man(wrong_guesses)
         display_hint(hint)
         guess = input("Enter a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input")
+            continue
+
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed")
+            continue
+
+        guessed_letters.add(guess)
+        
+
+        if guess in answer:
+            for i in range(len(answer)): 
+                if answer[i] == guess:
+                    hint[i] = guess
+
+        else: 
+            wrong_guesses += 1 
+
+        if "_" not in hint: 
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU WIN!")
+            is_running = False
+
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU LOSE!")
+            is_running = False
 
 if __name__ == "__main__":
     main() 
