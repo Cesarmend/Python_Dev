827e75ce893366f2e5afe261d8e28b3afaf8b1d3
Cesar <mendezcesarpablo@gmail.com>
2025-06-11 23:59:45 -0500

    reponse

--- Functionalities/API_pokedata_req.py
+++ Functionalities/API_pokedata_req.py
@@ -5,7 +5,10 @@ base_url = "https://pokeapi.co/api/v2/"
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
     response = requests.get(url)
-    print(response)
+    # print(response)
+
+    if response.status_code == 200:
+        pass
 
 pokemon_name ="pikachu"
 get_pokemon_info(pokemon_name)
