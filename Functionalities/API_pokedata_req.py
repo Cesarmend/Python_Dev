288059e925c25246a0a65d8adc366a85fff71c19
Cesar <mendezcesarpablo@gmail.com>
2025-06-12 22:05:50 -0500

    pokemon API requests

--- Functionalities/API_pokedata_req.py
+++ Functionalities/API_pokedata_req.py
@@ -5,13 +5,21 @@ base_url = "https://pokeapi.co/api/v2/"
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
     response = requests.get(url)
-    # print(response)
+    print(response)
 
     if response.status_code == 200:
         pokemon_data = response.json()
-        return pokemon_data
+        return pokemon_data 
     else:
         print(f"Failed to retrieve data {response.status_code}")
 
-pokemon_name ="pikachu"
-get_pokemon_info(pokemon_name)
+pokemon_name ="Kommo-o"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info['name'].capitalize()}")
+    print(f"Id: {pokemon_info['id']}")
+    types = [t['type']['name'] for t in pokemon_info['types']]
+    print("Types:", ", ".join(types).capitalize())
+    print(f"Height: {pokemon_info['height']}")
+    print(f"Weight: {pokemon_info['weight']}")
