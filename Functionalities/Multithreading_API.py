f94738c62bf87cea241b66fd06d28c8cd6e7f209
Cesar <mendezcesarpablo@gmail.com>
2025-06-25 18:36:26 -0500

    multithreading

--- Functionalities/Multithreading_API.py
+++ Functionalities/Multithreading_API.py
@@ -6,9 +6,9 @@
 import threading
 import time
 
-def walk_dog():
+def walk_dog(first, last):
     time.sleep(8)
-    print("You finish walking the dog")
+    print(f"You finish walking {first} {last}")
 
 def take_out_trash():
     time.sleep(2)
@@ -18,6 +18,17 @@ def get_mail():
     time.sleep(4)
     print("You get the mail")
 
-walk_dog()
-take_out_trash()
-get_mail()
+chore1 = threading.Thread(target=walk_dog, args=("Scooby","Doo"))
+chore1.start()
+
+chore2 = threading.Thread(target=take_out_trash)
+chore2.start()
+
+chore3 = threading.Thread(target=get_mail)
+chore3.start()
+
+chore1.join()
+chore1.join()
+chore1.join()
+
+print("All chores are complete!")
