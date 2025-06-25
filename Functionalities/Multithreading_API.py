f1e908e1f7c1e6a000b21bb0372bf486f4dfac22
Cesar <mendezcesarpablo@gmail.com>
2025-06-25 18:19:16 -0500

    steps

---
+++ Functionalities/Multithreading_API.py
@@ -0,0 +1,23 @@
+# multithreading = Used to perfom multiple tasks concurrently (multitasking)
+#                  Good for I/O bound tasks like reading files or fetching data from
+#                  APIs threading. Thread(target = my_function)
+
+
+import threading
+import time
+
+def walk_dog():
+    time.sleep(8)
+    print("You finish walking the dog")
+
+def take_out_trash():
+    time.sleep(2)
+    print("You take out the trash")
+
+def get_mail():
+    time.sleep(4)
+    print("You get the mail")
+
+walk_dog()
+take_out_trash()
+get_mail()
